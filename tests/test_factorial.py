1a372224e309e46dd5c4a1b36ac589b4fe478485
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-02-07 17:16:29 +0200

    Assert factorial(0) == 1

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,8 +1,12 @@
 from main import factorial
 
 
-def test_nothing():
+def test_factorial_for_natural_numbers():
      assert factorial(1) == 1
      assert factorial(2) == 2
      assert factorial(3) == 6
      assert factorial(4) == 24
+
+
+def test_factorial_at_critical_points():
+     assert factorial(0) == 1
